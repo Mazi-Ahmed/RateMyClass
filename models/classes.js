e7f9b9fe8c64b71658fc8f9d0ac2e4a5491fc31e
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-06-24 02:43:52 -0400

    Added Index and Show Routes

--- models/classes.js
+++ models/classes.js
@@ -1,37 +1,37 @@
 const classes = [
     {
-        class: 'IBM Full Stack Software Developer',
-        quaility: 4.6,
+        name: 'IBM Full Stack Software Developer',
+        quality: 4.6,
         difficulty: 2.0,
-        offered: IBM, 
+        offered: "IBM", 
         url: "https://www.coursera.org/professional-certificates/ibm-full-stack-cloud-developer",
-        review: "I graduated recently, and I am current;y looking and applying to jobs on the software field. I don't have any work experience in my field and I needed IBM Full Stack Software Developer Professional Certificate to pump my resume up. It helped me learn and understand how things are being done in the field today with the hands-on project."
+        review: "I graduated recently, and I am currently looking and applying to jobs on the software field. I don't have any work experience in my field and I needed IBM Full Stack Software Developer Professional Certificate to pump my resume up. It helped me learn and understand how things are being done in the field today with the hands-on project."
     }, {
-        class: 'Meta Front-End Developer',
-        quaility: 4.7,
+        name: 'Meta Front-End Developer',
+        quality: 4.7,
         difficulty: 2.0,
-        offered: Meta,
+        offered: "Meta",
         url: "https://www.coursera.org/professional-certificates/meta-front-end-developer",
         review: "The experience from taking certificates from Coursera was good. The Meta Front-End Developer Professional Certificate helped me tie together a lot of concepts I had been learning about and worked with in the past. It has given me confidence to start my own projects and apply for full-stack positions."
     }, {
-        class: 'Microsoft Business Analyst Professional',
-        quaility: 4.6,
+        name: 'Microsoft Business Analyst Professional',
+        quality: 4.6,
         difficulty: 2.0,
-        offered: Microsoft,
+        offered: "Microsoft",
         url: "https://www.coursera.org/professional-certificates/microsoft-business-analyst",
         review: 'This is the real deal. This class helped me advance in my career as an analyst.'
     }, {
-        class: 'Software Engineering Bootcamp',
-        quaility: 5.0,
+        name: 'Software Engineering Bootcamp',
+        quality: 5.0,
         difficulty: 4.0,
-        offered: GA, 
+        offered: "General Assembly", 
         url: "https://generalassemb.ly/students/courses/software-engineering-bootcamp/new-york-city",
         review: "Amazing and Inspirational"
     }, {
-        class: 'Swimming Lessons',
-        quaility: 5.0,
+        name: 'Swimming Lessons',
+        quality: 5.0,
         difficulty: 3.0,
-        offered: SwimEasyNY, 
+        offered: "SwimEasyNY", 
         url: "https://swimeasyny.com",
         review: "Swim Easy is a great place for anyone to learn how to swim. Having grown up with a fear of water, I was never able to learn, yet after just a few short months of attending classes, I have been able to get over my fear and am getting better every time. Krysztof, Devon and Matteo are all awesome and make it fun. I highly recommend this swim school to anyone."
     }
---
+++ server.js
@@ -0,0 +1,35 @@
+// DEPENDENCIES
+const express = require('express');
+const app = express();
+const port = 3000;
+// const methodOverride = require('method-override')
+
+// DATABASE
+const classes = require("./models/classes")
+
+// MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+// app.use(methodOverride('_method'))
+
+// ROUTES
+
+    // INDEX
+app.get("/classes/", (req,res)=>{
+    res.render("index.ejs", {
+        allClasses: classes,
+    })
+})
+
+    // SHOW
+app.get('/classes/:id', (req,res)=> {
+    res.render("show.ejs", {
+        classData: classes[req.params.id]
+    })
+})
+
+// PORT
+app.listen(port, ()=> {
+    console.log(`Listening on `, port)
+})
